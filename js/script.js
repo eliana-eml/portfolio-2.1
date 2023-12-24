3d0aa35691bc779cb93106092ffc2291333603f8
eliana-eml <eml.eliana57@hotmail.com>
2023-12-24 11:11:35 -0300

    Criação da divisão de Certificações, parte da linha do tempo Educacional

--- js/script.js
+++ js/script.js
@@ -4,20 +4,27 @@ function exibirLinhaDoTempoEducacao() {
 
     let linhaTempoEducacao = document.querySelector('.linha-do-tempo-educacao');
     let linhaTempoProfissional = document.querySelector('.linha-do-tempo-profissional');
+    let certificacoes = document.querySelector('.certificacoes');
+
     document.getElementById('educacao').style.color = "#2d0064";
     document.getElementById('profissional').style.color = "#afa2c2";
 
     linhaTempoEducacao.style.display = "block";
     linhaTempoProfissional.style.display = "none";
+    certificacoes.style.display = "flex";
+
 }
 
 function exibirLinhaDoTempoProfissional() {
 
     let linhaTempoEducacao = document.querySelector('.linha-do-tempo-educacao');
     let linhaTempoProfissional = document.querySelector('.linha-do-tempo-profissional');
+    let certificacoes = document.querySelector('.certificacoes');
+    
     document.getElementById('educacao').style.color = "#afa2c2";
     document.getElementById('profissional').style.color = "#2d0064";
     
     linhaTempoProfissional.style.display = "block";
     linhaTempoEducacao.style.display = "none";
+    certificacoes.style.display = "none";
 }
