7b8f2c1ac83d11194e259ef9645317519c78f73f
eliana-eml <eml.eliana57@hotmail.com>
2024-02-10 13:36:26 -0300

    Criação do menu lateral para a responsividade mobile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+function openNav() {
+    document.getElementById("nav-mobile-lista").style.width = "270px";
+}
+
+function closeNav() {
+    document.getElementById("nav-mobile-lista").style.width = "0";
+}
+
 window.onload = exibirLinhaDoTempoEducacao();
 
 function exibirLinhaDoTempoEducacao() {
